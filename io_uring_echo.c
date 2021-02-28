a338699c3c92e5d4476c349e1da408ee291ac489
s.suzugamine <s.suzugamine@gmail.com>
2021-02-28 17:29:45 +0900

    rm automatic buffers

--- io_uring_echo.c
+++ io_uring_echo.c
@@ -14,21 +14,18 @@
 #define N_ENTRY 2048
 #define GID 1
 
+int listen_fd;
+
 enum {
   ACCEPT,
   READ,
   WRITE,
-  PROVIDE_BUFFER,
 };
 
 typedef struct UserData {
   __u32 fd;
   __u16 type;
-  __u16 bid;
 } UserData;
-
-int listen_fd;
-char bufs[N_CLIENT][BUF_SIZE] = {0};
 
 void int_handle(int n) {
   close(listen_fd);
@@ -51,6 +48,8 @@ ssize_t write_n(int fd, char *ptr, size_t n) {
 }
 
 int main(int argc, char **argv) {
+  char buf[BUF_SIZE] = {0};
+
   // Create listen socket
   if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     fprintf(stderr, "Error: socket\n");
@@ -97,17 +96,6 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
 
-  // Setup automatic buffers
-  sqe = io_uring_get_sqe(&ring);
-  io_uring_prep_provide_buffers(sqe, bufs, BUF_SIZE, N_CLIENT, GID, 0);
-  io_uring_submit(&ring);
-  io_uring_wait_cqe(&ring, &cqe);
-  if (cqe->res < 0) {
-    fprintf(stderr, "Error: setup buffer %d\n", cqe->res);
-    return EXIT_FAILURE;
-  }
-  io_uring_cqe_seen(&ring, cqe);
-
   // Setup first accept
   sqe = io_uring_get_sqe(&ring);
   io_uring_prep_accept(sqe, listen_fd, (struct sockaddr *)&client_addr,
@@ -120,100 +108,72 @@ int main(int argc, char **argv) {
   memcpy(&sqe->user_data, &conn_info, sizeof(conn_info));
 
   while (1) {
-    io_uring_submit_and_wait(&ring, 1);
-    unsigned int head;
-    unsigned int cnt = 0;
+    io_uring_submit(&ring);
+    io_uring_wait_cqe(&ring, &cqe);
+    struct UserData conn_info;
+    memcpy(&conn_info, &cqe->user_data, sizeof(conn_info));
+    int type = conn_info.type;
 
-    // foreach in CEQ
-    io_uring_for_each_cqe(&ring, head, cqe) {
-      cnt++;
-      struct UserData conn_info;
-      memcpy(&conn_info, &cqe->user_data, sizeof(conn_info));
-      int type = conn_info.type;
-
-      if (cqe->res == -ENOBUFS) {
-        fprintf(stderr, "Error: no buffer %d\n", cqe->res);
-        close(listen_fd);
-        return EXIT_FAILURE;
-      } else if (type == PROVIDE_BUFFER) {
-        if (cqe->res < 0) {
-          fprintf(stderr, "Error: provide buffer %d\n", cqe->res);
-          close(listen_fd);
-          return EXIT_FAILURE;
-        }
-      } else if (type == ACCEPT) {
-        int conn_fd = cqe->res;
-        if (conn_fd >= 0) {  // no error
-          // Read from client
-          sqe = io_uring_get_sqe(&ring);
-          io_uring_prep_recv(sqe, conn_fd, NULL, BUF_SIZE, 0);
-          io_uring_sqe_set_flags(sqe, IOSQE_BUFFER_SELECT);
-          sqe->buf_group = GID;
-
-          UserData read_info = {
-              .fd = conn_fd,
-              .type = READ,
-          };
-          memcpy(&sqe->user_data, &read_info, sizeof(read_info));
-        }
-
-        // Add new client
+    if (cqe->res == -ENOBUFS) {
+      fprintf(stderr, "Error: no buffer %d\n", cqe->res);
+      close(listen_fd);
+      return EXIT_FAILURE;
+    } else if (type == ACCEPT) {
+      int conn_fd = cqe->res;
+      printf("Accept socket %d \n", conn_fd);
+      if (conn_fd >= 0) {  // no error
+        // Read from client
         sqe = io_uring_get_sqe(&ring);
-        io_uring_prep_accept(sqe, listen_fd, (struct sockaddr *)&client_addr,
-                             &client_len, 0);
-        io_uring_sqe_set_flags(sqe, 0);
-        UserData conn_info = {
-            .fd = listen_fd,
-            .type = ACCEPT,
-        };
-        memcpy(&sqe->user_data, &conn_info, sizeof(conn_info));
-      } else if (type == READ) {
-        int n_byte = cqe->res;
-        if (cqe->res <= 0) {  // connection closed by client
-          printf("Close socket %d\n", conn_info.fd);
-          close(conn_info.fd);
-        } else {
-          // Add Write
-          printf("Read %d bytes from socket %d\n", n_byte, conn_info.fd);
-          int bid = cqe->flags >> IORING_CQE_BUFFER_SHIFT;
-          sqe = io_uring_get_sqe(&ring);
-          io_uring_prep_send(sqe, conn_info.fd, &bufs[bid], n_byte, 0);
-          write_n(1, (char *)&bufs[bid], n_byte);
-          io_uring_sqe_set_flags(sqe, 0);
-          UserData write_info = {
-              .fd = conn_info.fd,
-              .type = WRITE,
-              .bid = bid,
-          };
-          memcpy(&sqe->user_data, &write_info, sizeof(write_info));
-        }
-      } else if (type == WRITE) {
-        // Add buffer
-        sqe = io_uring_get_sqe(&ring);
-        io_uring_prep_provide_buffers(sqe, bufs[conn_info.bid], BUF_SIZE, 1,
-                                      GID, conn_info.bid);
-        UserData provide_buffer_info = {
-            .fd = 0,
-            .type = PROVIDE_BUFFER,
-        };
-        memcpy(&sqe->user_data, &provide_buffer_info,
-               sizeof(provide_buffer_info));
-
-        // Add read
-        sqe = io_uring_get_sqe(&ring);
-        io_uring_prep_recv(sqe, conn_info.fd, NULL, BUF_SIZE, 0);
-        io_uring_sqe_set_flags(sqe, IOSQE_BUFFER_SELECT);
-        sqe->buf_group = GID;
+        io_uring_prep_recv(sqe, conn_fd, buf, BUF_SIZE, 0);
 
         UserData read_info = {
-            .fd = conn_info.fd,
+            .fd = conn_fd,
             .type = READ,
         };
         memcpy(&sqe->user_data, &read_info, sizeof(read_info));
       }
+
+      // Add new client
+      sqe = io_uring_get_sqe(&ring);
+      io_uring_prep_accept(sqe, listen_fd, (struct sockaddr *)&client_addr,
+                           &client_len, 0);
+      io_uring_sqe_set_flags(sqe, 0);
+      UserData conn_info = {
+          .fd = listen_fd,
+          .type = ACCEPT,
+      };
+      memcpy(&sqe->user_data, &conn_info, sizeof(conn_info));
+    } else if (type == READ) {
+      int n_byte = cqe->res;
+      if (cqe->res <= 0) {  // connection closed by client
+        printf("Close socket %d\n", conn_info.fd);
+        close(conn_info.fd);
+      } else {
+        // Add Write
+        printf("Read %d bytes from socket %d\n", n_byte, conn_info.fd);
+        sqe = io_uring_get_sqe(&ring);
+        io_uring_prep_send(sqe, conn_info.fd, buf, n_byte, 0);
+        write_n(1, buf, n_byte);  // output stdout
+        io_uring_sqe_set_flags(sqe, 0);
+        UserData write_info = {
+            .fd = conn_info.fd,
+            .type = WRITE,
+        };
+        memcpy(&sqe->user_data, &write_info, sizeof(write_info));
+      }
+    } else if (type == WRITE) {
+      // Add read
+      sqe = io_uring_get_sqe(&ring);
+      io_uring_prep_recv(sqe, conn_info.fd, buf, BUF_SIZE, 0);
+
+      UserData read_info = {
+          .fd = conn_info.fd,
+          .type = READ,
+      };
+      memcpy(&sqe->user_data, &read_info, sizeof(read_info));
     }
 
-    io_uring_cq_advance(&ring, cnt);
+    io_uring_cqe_seen(&ring, cqe);
   }
 
   close(listen_fd);
